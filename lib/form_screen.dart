ac6171ba36106f3284bc6b416c463f8a2bd4642d
Yeard <yeardmorshed1999@gmail.com>
2023-08-12 22:28:05 +0600

    V 1.0.2
    Form Screen Done

---
+++ lib/form_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({super.key});
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Form with Validation',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your email';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _phoneController,
+                  decoration: const InputDecoration(labelText: 'Phone'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your phone number';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a password';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  obscureText: true,
+                  decoration:
+                      const InputDecoration(labelText: 'Confirm Password'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please confirm your password';
+                    }
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print('successfully done');
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:plan_it/task_board_screen.dart';
 
+import 'form_screen.dart';
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(body: SizedBox(
+    return Scaffold(
+        body: SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
           const Spacer(),
-         const Text('Welcome To',style: TextStyle(fontSize: 15,),textAlign: TextAlign.center),
-       const   Text('Plan IT',style:  TextStyle(fontSize: 40,fontWeight: FontWeight.w900),),
+          const Text('Welcome To',
+              style: TextStyle(
+                fontSize: 15,
+              ),
+              textAlign: TextAlign.center),
+          const Text(
+            'Plan IT',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+          ),
           const Spacer(),
-        const  SizedBox(
+          const SizedBox(
               width: 250,
-              child:  Text('Your personal Task Management and planning solution ',style: TextStyle(fontSize: 15,),textAlign: TextAlign.center)),
- const SizedBox(height: 20,),
-
-          Material(color: Colors.black54,
-          clipBehavior: Clip.hardEdge,
-          borderRadius: BorderRadius.circular(10),
+              child:
+                  Text('Your personal Task Management and planning solution ',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                      textAlign: TextAlign.center)),
+          const SizedBox(
+            height: 20,
+          ),
+          Material(
+            color: Colors.black54,
+            clipBehavior: Clip.hardEdge,
+            borderRadius: BorderRadius.circular(10),
             child: InkWell(
-              onTap: (){
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => TaskBoardScreen()),
@@ -34,20 +50,44 @@ class Home extends StatelessWidget {
               },
               child: const SizedBox(
                   height: 50,
-
                   child: Padding(
-                    padding: EdgeInsets.only(left: 40,right: 40,top: 15,bottom: 15),
-                    child: Text("Let's get started",style: TextStyle(fontSize: 15,color: Colors.white),),
-
-
+                    padding: EdgeInsets.only(
+                        left: 40, right: 40, top: 15, bottom: 15),
+                    child: Text(
+                      "Let's get started",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
                   )),
             ),
-
-
           ),
-
+          const SizedBox(
+            height: 20,
+          ),
+            /// form
+          Material(
+            color: Colors.blue,
+            clipBehavior: Clip.hardEdge,
+            borderRadius: BorderRadius.circular(10),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FormScreen()),
+                );
+              },
+              child: const SizedBox(
+                  height: 50,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        left: 40, right: 40, top: 15, bottom: 15),
+                    child: Text(
+                      "Form",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                  )),
+            ),
+          ),
           const Spacer(),
-
         ],
       ),
     ));
